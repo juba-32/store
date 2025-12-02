cdd28052be13f692f055503e2ea3c670d087ecac
ahmed bendary <bendarya58@gmail.com>
2025-12-02 18:48:29 +0200

    sssss

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,10 @@
 import Bannar from '../components/banner/Bannar'
-import CategorySlider from '../components/hero/CategorySlider'
 import Hero from '../components/hero/Hero'
 
 export default function Home() {
   return (
     <div>
       <Hero/>
-      {/* <CategorySlider/> */}
       <Bannar />
     </div>
   )
