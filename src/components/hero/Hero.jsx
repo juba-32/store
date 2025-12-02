d3489564be04acc88b89a585b772ed6da473272b
ahmed bendary <bendarya58@gmail.com>
2025-12-02 19:31:17 +0200

    sssss

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -33,7 +33,7 @@ export default function Hero() {
       className="hero-section"
       style={{
         backgroundImage:
-          `linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.1)), url(${"/images/storeBG.webp"})`,
+          `linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.1)), url(${"/images/storeBG.avif"})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
